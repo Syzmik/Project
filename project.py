c2239597242c46ae45ded1d9900527fa3f2a7307
Michael Burress <Syzmik117@gmail.com>
2022-10-17 12:53:30 -0400

    Rework options tree

--- project.py
+++ project.py
@@ -3,4 +3,13 @@ print( "Enter user name:")
 Name = input()
 
 print( "Hello,", Name )
-open( Name, 'w') 
+try:
+    open( Name, 'x')
+except FileExistsError:
+        pass
+        
+print( "What would you like to do ? add, delete, view or edit")
+input() == Choice)
+if Choice == add :
+    print( "Write what the next task is, then press enter.")
+    
