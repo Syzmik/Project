0980ce9f2731ca712497242cf16cca0bdcf3dd44
Michael Burress <Syzmik117@gmail.com>
2022-10-17 14:25:17 -0400

    Major update, "add" functionality added and tested with new line appending. not bug tested.

--- project.py
+++ project.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import fileinput
 print( "Enter user name:")
 Name = input()
 
@@ -8,8 +10,35 @@ try:
 except FileExistsError:
         pass
         
-print( "What would you like to do ? add, delete, view or edit")
-input() == Choice)
-if Choice == add :
-    print( "Write what the next task is, then press enter.")
+options = ['add', 'complete', 'edit' ,'remove', 'list', 'exit']
+
+user_input = ' '
+
+input_message = "pick an option:\n"
+
+for index, item in enumerate(options):
+    input_message += f'{index+1}) {item}\n'
     
+input_message += "Your Choice: "
+
+while user_input.lower() not in options:
+    user_input = input(input_message)
+user_input == input
+
+if user_input == "add":
+    print("Type task to be added")
+fd = open(Name,"a")
+fd.write(input())
+fd.write("\n")
+
+if user_input == "list":
+    filepath = Name
+with open(filepath) as fp:
+   line = fp.readline()
+   cnt = 1
+   while line:
+       print("Line {}: {}".format(cnt, line.strip()))
+       line = fp.readline()
+       cnt += 1
+    
+
